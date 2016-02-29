054b5eed36050219d18f1a9ce2f8daea1dce3cb4
ben-cooper <ben0cooper@gmail.com>
2016-02-28 22:54:16 -0500

    Added another test case for feature 3.

--- D3/Bugs/feature3test.py
+++ D3/Bugs/feature3test.py
@@ -20,7 +20,7 @@ class EnvironVarTest(unittest.TestCase):
             self.assertEqual(f3._get_home(), os.path.expanduser("~"),
             "unix value overridden")
 
-    def test_with_var(self):
+    def test_with_var_valid_path(self):
         #setting variable in windows
         if sys.platform == "win32":
             os.environ["MATPDIR"] == "C:"
@@ -30,6 +30,24 @@ class EnvironVarTest(unittest.TestCase):
 
         self.assertEqual(f3._get_home(), os.environ.get('MATPDIR'),
         "MATPDIR was not read")
+
+    def test_with_var_invalid_path(self):
+        os.environ["MATPDIR"] = "_________________"
+
+        #linux test
+        if sys.platform == 'linux':
+            self.assertEqual(f3._get_home(), os.path.expanduser("~"),
+            "linux value overridden with invalid path")
+        #windows
+        elif sys.platform == "win32":
+            self.assertEqual(f3._get_gome(), 
+            os.path.expanduser(b"~").decode(sys.getfilesystemencoding()),
+            "windows value overridden with invalid path")
+        #unix
+        else:
+            self.assertEqual(f3._get_home(), os.path.expanduser("~"),
+            "unix value overridden with invalid path")
+
 
 
 if __name__ == "__main__":
