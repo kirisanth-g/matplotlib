89dbf4f7ac5d2d84e649ea7c834429c68703dd72
kiwi <kiwi@kiwi-strawberry-machine.Workgroup>
2016-03-27 18:04:00 -0400

    Added test files for fig

---
+++ D5/new_fig_test.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+n = plt.figure("fig 1")
+o = plt.figure("fig 2")
+
+ax1 = plt.subplot2grid((3,3),(0,0))
+ax2 = plt.subplot2grid((3,3),(0,1), colspan=2)
+ax3 = plt.subplot2grid((3,3),(1,2), rowspan=2, fig=n)
+ax4 = plt.subplot2grid((3,3),(2,0), fig = n)
+plt.show()
